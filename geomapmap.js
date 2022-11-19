e8a38d15964303042e57185e362191fdbd3c79b5
Cromwell De Guzman <55942254+cdeguzman1208@users.noreply.github.com>
2022-11-18 16:58:46 -0800

    Update geomapmap.js

--- geomapmap.js
+++ geomapmap.js
@@ -22,7 +22,79 @@ var svg = d3.select("body")
 .attr("height", height + margin.top + margin.bottom)
 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// helper function for reading in dataset
+// define data values
+var density = d3.map();
+var name = d3.map();
+
+// define path
+var path = d3.geoPath();
+
+// define color
+var color = d3.scaleThreshold()
+    .domain([1, 10, 50, 200, 500, 1000, 2000, 4000])
+    .range(d3.schemeBlues[9]);
+
+// define x
+var x = d3.scaleSqrt()
+    .domain([0, 4500])
+    .rangeRound([440, 950]);
+
+// define g
+var g = svg.append("g")
+    .attr("class", "key")
+    .attr("transform", "translate(0,40)");
+
+// make legend
+g.selectAll("rect")
+  .data(color.range().map(function(d) {
+      d = color.invertExtent(d);
+      if (d[0] == null) d[0] = x.domain()[0];
+      if (d[1] == null) d[1] = x.domain()[1];
+      return d;
+    }))
+  .enter().append("rect")
+    .attr("height", 8)
+    .attr("x", function(d) { return x(d[0]); })
+    .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+    .attr("fill", function(d) { return color(d[0]); });
+
+// add text to legend
+g.append("text")
+    .attr("class", "caption")
+    .attr("x", x.range()[0])
+    .attr("y", -6)
+    .attr("fill", "#000")
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .text("Population per square mile");
+
+// show legend tick lines
+g.call(d3.axisBottom(x)
+    .tickSize(13)
+    .tickValues(color.domain()))
+  .select(".domain")
+    .remove();
+
+// draw geomap
+d3.json("topo.json", function(error, topology) {
+  if (error) throw error;
+
+  svg.append("g")
+    .selectAll("path")
+    .data(topojson.feature(topology, topology.objects.tracts).features)
+    .enter().append("path")
+      .attr("fill", function(d) { return color(d.properties.density); })
+      .attr("d", path);
+
+  svg.append("path")
+      .datum(topojson.feature(topology, topology.objects.counties))
+      .attr("fill", "none")
+      .attr("stroke", "#000")
+      .attr("stroke-opacity", 0.3)
+      .attr("d", path);
+});
+
+// helper function for reading in population density dataset
 function rowConverter(data) {
     return {
         geoID : data.geoID,
@@ -35,12 +107,11 @@ function rowConverter(data) {
     }
 }
 
-// read in dataset
+// display population density
 d3.csv("rhodeisland.csv",rowConverter).then(function(data) {
     
-    // check if actually reading dataset correctly
+    // check if actually reading in dataset correctly
     dataset = data;
     console.table(dataset, ["geoID", "geoID2", "state", "targetID", "targetID2", "name", "density"]);
-    
     
 });
